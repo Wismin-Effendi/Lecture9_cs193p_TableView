c8f2f91bd73d29ed53397a87ed72d51202874265
Wismin Effendi <wismin.effendi@gmail.com>
2017-06-24 19:49:01 -0500

    Before integration with Twitter Framework.

---
+++ Smashtag/TwitterTableViewController.swift
@@ -0,0 +1,54 @@
+//
+//  TwitterTableViewController.swift
+//  Smashtag
+//
+//  Created by Wismin Effendi on 6/24/17.
+//  Copyright © 2017 iShinobi. All rights reserved.
+//
+
+import UIKit
+
+class TwitterTableViewController: UITableViewController {
+    
+    private var tweets = [Array<Tweet>]()
+    
+    var searchText: String? {
+        didSet {
+            tweets.removeAll()
+            tableView.reloadData()
+            searchForTweets()
+            title = searchTest
+        }
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        searchText = "#stanford"
+    }
+
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    /*
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+  
+}
